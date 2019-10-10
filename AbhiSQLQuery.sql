e52bee390daf0ed7d5ddf2f6725c9a24bc9f9956
Basant Kumar <basantmailbox@gmail.com>
2019-10-10 17:09:25 +0530

    Update AbhiSQLQuery.sql

--- AbhiSQLQuery.sql
+++ AbhiSQLQuery.sql
@@ -59,12 +59,15 @@ END -- End Function
 
 
 
+
+
+
 ALTER PROCEDURE InsertSplitString
 @strValue nvarchar(MAX)
 AS
 DECLARE @FinalSplitTbl TABLE (FsId INT IDENTITY(1,1),C1 VARCHAR(100), C2 VARCHAR(100),C3 VARCHAR(100),C4 VARCHAR(100),C5 VARCHAR(100)) 
 DECLARE @FirstSplitTbl TABLE (FsId INT IDENTITY(1,1),C1 VARCHAR(MAX))
-DECLARE @SecondSplitTbl TABLE (FsId INT IDENTITY(1,1),C1 VARCHAR(MAX))
+
 INSERT INTO @FirstSplitTbl (C1) 
 SELECT Item from Split(@strValue,',')
 
@@ -83,69 +86,71 @@ DECLARE @Col4 VARCHAR(MAX)
 DECLARE @Col5 VARCHAR(MAX)
 DECLARE @ContactId INT
 
-SELECT * FROM  @FirstSplitTbl
 
+CREATE TABLE #SecondSplitTbl(FsId INT IDENTITY(1,1),C1 VARCHAR(MAX))
 
 WHILE @i < @CNT_TOTAL
 BEGIN
     SET @i = @i + 1
 	SET @Total_Inner=0
-	delete from  @SecondSplitTbl
 	select @FirstSplitRow= C1 FROM @FirstSplitTbl WHERE FsId=@i
-	INSERT INTO @SecondSplitTbl (C1) SELECT * FROM Split(@FirstSplitRow,'|')
-	SELECT @Total_Inner=COUNT(*) FROM @SecondSplitTbl
+	INSERT INTO #SecondSplitTbl (C1) SELECT * FROM Split(@FirstSplitRow,'|')
+	SELECT @Total_Inner=COUNT(*) FROM #SecondSplitTbl
 	
+	--SELECT * FROM #SecondSplitTbl
 	SET @inner=0
-	SELECt @Total_Inner
 			WHILE @inner < @Total_Inner
 			BEGIN
 			SELECT @inner
 				SET @inner = @inner + 1
 				IF (@inner=1)    
 				BEGIN
-				select @Col1=C1 FROM @SecondSplitTbl WHERE FsId=@inner
+				select @Col1=C1 FROM #SecondSplitTbl WHERE FsId=@inner
 				END
 				
 				IF (@inner=2)    
 				BEGIN
-				select @Col2=C1 FROM @SecondSplitTbl WHERE FsId=@inner
+				select @Col2=C1 FROM #SecondSplitTbl WHERE FsId=@inner
 				END
 
 				IF (@inner=3)    
 				BEGIN
-				select @Col3=C1 FROM @SecondSplitTbl WHERE FsId=@inner
+				select @Col3=C1 FROM #SecondSplitTbl WHERE FsId=@inner
 				END
 
 				IF (@inner=4)    
 				BEGIN
-				select @Col4=C1 FROM @SecondSplitTbl WHERE FsId=@inner
+				select @Col4=C1 FROM #SecondSplitTbl WHERE FsId=@inner
 				END
 
 				IF (@inner=5)    
 				BEGIN
-				select @Col5=C1 FROM @SecondSplitTbl WHERE FsId=@inner
+				select @Col5=C1 FROM #SecondSplitTbl WHERE FsId=@inner
 				END
 
 			END
+				--DROP TABLE #SecondSplitTbl
 
+				TRUNCATE TABLE #SecondSplitTbl
 
-			select @ContactId=id from contacts where wano= @Col1 and contactno= @Col2;
+			--select @ContactId=id from contacts where wano= @Col1 and contactno= @Col2;
 
-			IF (@ContactId>0)  
-				BEGIN
-				INSERT INTO MEDIA_HEX (CONTACTID,MEDIA_URL,MEDIA_MIME_TYPE,THUMB_IMAGE) VALUES (@ContactId,@Col3,@Col4,@Col5)
-				END
-				ELSE
-				BEGIN
+			--IF (@ContactId>0)  
+			--	BEGIN
+			--	INSERT INTO MEDIA_HEX (CONTACTID,MEDIA_URL,MEDIA_MIME_TYPE,THUMB_IMAGE) VALUES (@ContactId,@Col3,@Col4,@Col5)
+			--	END
+			--	ELSE
+			--	BEGIN
 
-				INSERT INTO CONTACTS (WANO,CONTACTNO) VALUES (@col1,@col2)
-				SET	@ContactId = SCOPE_IDENTITY()
-				INSERT INTO MEDIA_HEX (CONTACTID,MEDIA_URL,MEDIA_MIME_TYPE,THUMB_IMAGE) VALUES (@ContactId,@Col3,@Col4,@Col5)
+			--	INSERT INTO CONTACTS (WANO,CONTACTNO) VALUES (@col1,@col2)
+			--	SET	@ContactId = SCOPE_IDENTITY()
+			--	INSERT INTO MEDIA_HEX (CONTACTID,MEDIA_URL,MEDIA_MIME_TYPE,THUMB_IMAGE) VALUES (@ContactId,@Col3,@Col4,@Col5)
 
-				END
+			--	END
 
 		INSERT INTO @FinalSplitTbl (C1,C2,C3,C4,C5) VALUES(@Col1,@Col2,@Col3,@Col4,@Col5)
 
+	
 END
 
 SELECT * FROM @FinalSplitTbl
@@ -155,9 +160,8 @@ GO
 
 --  EXEC InsertSplitString '918403082585|919555590000|fgj4jkj|fjgkj666|iuituri,918403082585|919555590000|222222|333333|4444444'
 
-
---   select * from MEDIA_HEX
-	-- delete   from MEDIA_HEX
+-- select * from MEDIA_HEX
+-- delete   from MEDIA_HEX
 
 
 
